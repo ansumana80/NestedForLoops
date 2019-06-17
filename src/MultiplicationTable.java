d19b515e951a27a98f4089e049949f0bdf5c6ab7
ansumana80 <ansurogersbusiness@gmail.com>
2019-06-17 17:20:50 -0400

    first commit

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,18 @@
+public class MultiplicationTable
+
+{
+
+    static final int MAX = 10;
+
+    public static void main(String[] args)
+    {
+        int row, column;
+
+        for (row = 1; row <= MAX; row++) {
+            for (column = 1; column <= MAX; column++) {
+                System.out.print(row * column + "\t");
+            }
+            System.out.println(); //Takes us to the next line
+        }
+    }
+}
